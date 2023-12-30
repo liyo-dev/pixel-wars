62f3739964527ddbf5b588b76d276aa35599dc6f
liyodev <luarbaz@gmail.com>
2023-12-30 20:54:05 +0100

    First Commit

---
+++ shared/constants.ts
@@ -0,0 +1,25 @@
+export const WIDTH = 64;
+export const HEIGHT = 64;
+
+export const PIXEL_SIZE = 32;
+
+export const COLORS_NAMES = {
+  red: "#f56c6c",
+  green: "#6cf56e",
+  blue: "#6cccf5",
+  yellow: "#f5f06c",
+  brightBlue: "#6c75f5",
+  pink: "#f56ce5",
+  white: "#FFFFFF",
+  black: "#000000",
+} as const;
+
+export const COLORS = Object.values(COLORS_NAMES);
+
+export const KEYS = {
+  tiles: "tiles",
+} as const;
+
+export const CHANNELS = {
+  PIXEL_UPDATE: "pixel-update",
+};
